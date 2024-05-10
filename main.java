09fe9a45f82ea4dcf3ca7cfd15e87bde54d0fc68
Jacobmmiv <93954528+Jacobmmiv@users.noreply.github.com>
2024-05-10 04:02:16 +0100

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) throws InterruptedException {
+
+            // Instantiate Flashcards object, because, well, we have to... or not and just leave
+            flashcards flashcards = new flashcards();
+            // Call the stuff() method, because apparently, that's what we do in this sad existence.
+            flashcards.stuff();
+
+    }
+}
